75467a823e86a86aefd1828ea96780a072a51859
johan/n <j.b.kleindopf@gmail.com>
2025-09-27 23:00:24 +0200

    ikfodnmojkfdngjfdng

--- ch2/ch2.py
+++ ch2/ch2.py
@@ -2,3 +2,15 @@ import torch
 
 
 
+x = torch.tensor([1.0, 2, 4, 8])
+y = torch.tensor([2, 2, 2, 2])
+
+X = torch.arange(12, dtype=torch.float32).reshape((3, 4))
+Y = torch.tensor([[2.0, 1, 4, 3], [1, 2, 3, 4], [4, 3, 2, 1]])
+
+Z = torch.zeros_like(Y)
+print(Z)
+print('id(Z): ', id(Z))
+Z[:] = X + Y
+print(Z)
+print('id(Z): ', id(Z))
